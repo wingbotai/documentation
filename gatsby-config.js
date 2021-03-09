ba5e393e3810c78163ce72cfa2b8c0ce388bea68
Zdenek Hasek <me@zdehasek.com>
2021-03-09 12:46:20 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: "Wingbot documentation",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "sasalele",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
